64da20578df261de16e91f781bf098fa8a6c082c
Ritam Jana <ritamjana2002@gmail.com>
2021-07-13 22:06:17 +0530

    first assignment initial commit

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/ritam/Documents/AUV/Software/Simulation/catkin_ws/src
+# Build directory: /home/ritam/Documents/AUV/Software/Simulation/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("teleop_twist_keyboard")
+subdirs("demo_bot_control")
+subdirs("demo_bot_gazebo")
